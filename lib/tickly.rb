5ab64ad7fad9fcbaea8b1e4a3f09585f5097739a
Julik Tarkhanov <me@julik.nl>
2013-03-01 02:13:55 +0100

    Use an Enumerable for expression containers

--- lib/tickly.rb
+++ lib/tickly.rb
@@ -1,6 +1,46 @@
 require File.dirname(__FILE__) + "/tickly/parser"
+require 'forwardable'
 
 module Tickly
+  
+  class Expr
+    extend Forwardable
+    
+    def_delegators :@e, :push, :<<, :any?, :reject!, :map!, :[], :delete_at, :include?, :each, :empty?, :join, :length
+    
+    def initialize(elements = [])
+      @e = elements
+    end
+    
+    def map(&blk)
+      self.class.new(@e.map(&blk))
+    end
+    
+    def to_a
+      @e
+    end
+    
+    def ==(another)
+      another.to_a == to_a
+    end
+  end
+  
+  # Represents an expression between curly braces (within which no text substitution will be done)
+  # like  { 1 2 3 }
+  class LiteralExpr < Expr
+    def inspect
+      "le%s" % @e.inspect
+    end
+  end
+
+  # Represents an expression between square brackets (where text substitution will be done)
+  # like  [1 2 3]
+  class StringExpr < Expr
+    def inspect
+      "se%s" % @e.inspect
+    end
+  end
+  
   def self.to_tcl(e)
     if e.is_a?(Tickly::LiteralExpr)
       '{%s}' % e.map{|e| to_tcl(e)}.join(' ')
