a93fb140775dd8316d4b689154910cb19b2bbf98
Julik Tarkhanov <me@julik.nl>
2013-03-15 04:31:43 +0100

    And more documentation

--- lib/tickly.rb
+++ lib/tickly.rb
@@ -6,7 +6,8 @@ require 'forwardable'
 module Tickly
   VERSION = '0.0.4'
   
-  # Provides the methods for quickly emitting the LiteralExpr and StringExpr objects
+  # Provides the methods for quickly emitting the expression arrays,
+  # is used in tests
   module Emitter
     def le(*elems)
       [:c] + elems
@@ -21,11 +22,15 @@ module Tickly
     end
   end
   
+  # Converts a passed Array (received from a Parser)
+  # into a TCL expression. This is only ever used in tests
   def self.to_tcl(e)
     if e.is_a?(Array) && e[0] == :c
       '{%s}' % e.map{|e| to_tcl(e)}.join(' ')
     elsif e.is_a?(Array) && e[0] == :b
       '[%s]' % e.map{|e| to_tcl(e)}.join(' ')
+    elsif e.is_a?(Array)
+      e.map{|e| to_tcl(e)}.join(" ")
     elsif e.is_a?(String) && (e.include?('"') || e.include?("'"))
       e.inspect
     else
