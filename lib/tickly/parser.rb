cc0b8933dca35ea7f8d586b59ae4fd7b12c81385
Julik Tarkhanov <me@julik.nl>
2013-03-15 20:19:20 +0100

    Greatly simplify multiple expressions
    Actually we just need two simple buffers

--- lib/tickly/parser.rb
+++ lib/tickly/parser.rb
@@ -45,99 +45,74 @@ module Tickly
     TERMINATORS = ["\n", ";"]
     ESC = 92.chr # Backslash (\)
     
+    # Package the expressions, stack and buffer.
+    # We use a special flag to tell us whether we need multuple expressions
+    # or not, if not we just discard them
+    def wrap_up(expressions, stack, buf, stack_depth, multiple_expressions)
+      stack << buf if (buf.length > 0)
+      return stack unless multiple_expressions
+      
+      expressions << stack if stack.any?
+      expressions.each { |expr| expand_subexpr!(expr, stack_depth + 1) }
+      
+      return expressions
+    end
+    
     # Parse from a passed IO object either until an unescaped stop_char is reached
     # or until the IO is exhausted. The last argument is the class used to
     # compose the subexpression being parsed. The subparser is reentrant and not
     # destructive for the object containing it.
     def sub_parse(io, stop_char = nil, stack_depth = 0)
       # A standard stack is an expression that does not evaluate to a string
+      expressions = []
       stack = []
       buf = ''
       last_char_was_linebreak = false
+      multiple_expressions = false
       
       no_eof do
         char = io.read_one_byte
-      
-        if buf[LAST_CHAR] != ESC
-          if char == stop_char # Bail out of a subexpr
-            stack << buf if (buf.length > 0)
-            # Chip away the tailing linebreak if it's there
-            chomp!(stack)
-            return stack
-          elsif char == " " || char == "\n" # Space
-            if buf.length > 0
-              stack << buf
-              buf = ''
-            end
-            if TERMINATORS.include?(char) # Introduce a stack separator! This is a new line
-              if stack.any? && !last_char_was_linebreak
-                last_char_was_linebreak = true
-                stack = handle_expr_terminator(stack, stack_depth)
-              end
-            end
-          elsif char == '[' # Opens a new string expression
-            stack << buf if (buf.length > 0)
-            last_char_was_linebreak = false
-            stack << [:b] + sub_parse(io, ']', stack_depth + 1)
-          elsif char == '{' # Opens a new literal expression  
-            stack << buf if (buf.length > 0)
-            last_char_was_linebreak = false
-            stack << [:c] + sub_parse(io, '}', stack_depth + 1)
-          elsif char == '"'
-            stack << buf if (buf.length > 0)
-            last_char_was_linebreak = false
-            stack << parse_str(io, '"')
-          elsif char == "'"
-            stack << buf if (buf.length > 0)
-            last_char_was_linebreak = false
-            stack << parse_str(io, "'")
+        
+        if char == stop_char # Bail out of a subexpr
+          # Handle any remaining subexpressions
+          return wrap_up(expressions, stack, buf, stack_depth, multiple_expressions)
+        elsif char == " " || char == "\n" # Space
+          if buf.length > 0
+            stack << buf
+            buf = ''
+          end
+          if TERMINATORS.include?(char) && stack.any? && !last_char_was_linebreak # Introduce a stack separator! This is a new line
+            stack << buf if buf.length > 0
+            expressions << stack
+            stack = []
+            last_char_was_linebreak = true
+            multiple_expressions = true
+            #puts "Next expression! #{expressions.inspect} #{stack.inspect} #{buf.inspect}"
           else
             last_char_was_linebreak = false
-            buf << char
           end
+        elsif char == '[' # Opens a new string expression
+          stack << buf if (buf.length > 0)
+          stack << [:b] + sub_parse(io, ']', stack_depth + 1)
+        elsif char == '{' # Opens a new literal expression  
+          stack << buf if (buf.length > 0)
+          stack << [:c] + sub_parse(io, '}', stack_depth + 1)
+        elsif char == '"'
+          stack << buf if (buf.length > 0)
+          stack << parse_str(io, '"')
+        elsif char == "'"
+          stack << buf if (buf.length > 0)
+          stack << parse_str(io, "'")
         else
-          last_char_was_linebreak = false
           buf << char
         end
       end
-    
-      # Ramass any remaining buffer contents
-      stack << buf if (buf.length > 0)
       
-      # Handle any remaining subexpressions
-      if stack.include?(nil)
-        stack = handle_expr_terminator(stack, stack_depth)
-      end
-      # Chip awiy the trailing null
-      chomp!(stack)
-      
-      return stack
+      return wrap_up(expressions, stack, buf, stack_depth, multiple_expressions)
     end
     
     def chomp!(stack)
       stack.delete_at(-1) if stack.any? && stack[-1].nil?
-    end
-    
-    def handle_expr_terminator(stack, stack_depth)
-      # Figure out whether there was a previous expr terminator
-      previous_i = stack.index(nil)
-      # If there were none, just get this over with. Wrap the stack contents
-      # into a subexpression and carry on.
-      unless previous_i
-        subexpr = stack
-        expand_subexpr!(subexpr, stack_depth + 1)
-        return [subexpr] + [nil]
-      end
-      
-      # Now, if there was one, we are the next subexpr in line that just terminated.
-      # What we need to do is pick out all the elements from that terminator onwards
-      # and wrap them.
-      subexpr = stack[previous_i+1..-1]
-      
-      # Use expand_subexpr! to trim away any fat that we don't need
-      expand_subexpr!(subexpr, stack_depth + 1)
-      
-      return stack[0...previous_i] + [subexpr] + [nil]
     end
     
     def no_eof(&blk)
