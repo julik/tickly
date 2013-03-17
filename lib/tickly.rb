d242174f2c4b36f4eb54654685818654499b1540
Julik Tarkhanov <me@julik.nl>
2013-03-17 12:32:45 +0100

    Tickly.to_tcl wasn't actually used

--- lib/tickly.rb
+++ lib/tickly.rb
@@ -8,7 +8,7 @@ module Tickly
   
   # Provides the methods for quickly emitting the expression arrays,
   # is used in tests
-  module Emitter
+  module Emitter #:nodoc :all
     def le(*elems)
       [:c] + elems
     end
@@ -21,22 +21,5 @@ module Tickly
       [:b] + elems
     end
   end
-  
-  # Converts a passed Array (received from a Parser)
-  # into a TCL expression. This is only ever used in tests
-  def self.to_tcl(e)
-    if e.is_a?(Array) && e[0] == :c
-      '{%s}' % e.map{|e| to_tcl(e)}.join(' ')
-    elsif e.is_a?(Array) && e[0] == :b
-      '[%s]' % e.map{|e| to_tcl(e)}.join(' ')
-    elsif e.is_a?(Array)
-      e.map{|e| to_tcl(e)}.join(" ")
-    elsif e.is_a?(String) && (e.include?('"') || e.include?("'"))
-      e.inspect
-    else
-      e.to_s
-    end
-  end
-  
   
 end
