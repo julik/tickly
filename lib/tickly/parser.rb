1a46ae2b86d70795d002a28c1a76193a656453fa
Julik Tarkhanov <me@julik.nl>
2013-03-22 19:38:37 +0100

    Dicked about. DId not succeed.

--- lib/tickly/parser.rb
+++ lib/tickly/parser.rb
@@ -1,3 +1,5 @@
+require 'strscan'
+
 module Tickly
   # Simplistic, incomplete and most likely incorrect TCL parser
   class Parser
@@ -15,11 +17,9 @@ module Tickly
     #   expression = expressions[0] #=> ["2", "2"]
     def parse(io_or_str)
       bare_io = io_or_str.respond_to?(:read) ? io_or_str : StringIO.new(io_or_str)
-      # Wrap the IO in a Bychar buffer to read faster
-      reader = Bychar::Reader.new(bare_io)
-      # Use multiple_expressions = true so that the top-level parsed script is always an array
-      # of expressions
-      sub_parse(reader, stop_char = nil, stack_depth = 0, multiple_expressions = true)
+      # Use multiple_expressions = true so that the top-level
+      # parsed script is always an array of expressions
+      sub_parse(bare_io, stop_char = nil, stack_depth = 0, multiple_expressions = true)
     end
     
     # Override this to remove any unneeded subexpressions.
@@ -32,19 +32,25 @@ module Tickly
     private
     
     LAST_CHAR = -1..-1 # If we were 1.9 only we could use -1
-    TERMINATORS = ["\n", ";"]
     ESC = 92.chr # Backslash (\)
+    QUOTES = %w( ' " )
     
     # Package the expressions, stack and buffer.
     # We use a special flag to tell us whether we need multuple expressions
     # or not, if not we just discard them
-    def wrap_up(expressions, stack, buf, stack_depth, multiple_expressions)
-      stack << buf if (buf.length > 0)
+    def wrap_up(expressions, stack, stack_depth, multiple_expressions)
       return stack unless multiple_expressions
       
       expressions << stack if stack.any?
       
       return expressions
+    end
+    
+    def tokens_to_stack(chunk, stack, chop_chars = 1)
+      range = 0..(-1 - chop_chars)
+      chunk[range].split(/\s+/).each do | bareword |
+        stack << bareword unless bareword.empty?
+      end
     end
     
     # Parse from a passed IO object either until an unescaped stop_char is reached
@@ -55,70 +61,75 @@ module Tickly
       # A standard stack is an expression that does not evaluate to a string
       expressions = []
       stack = []
-      buf = ''
-      last_char_was_linebreak = false
       
-      no_eof do
-        char = io.read_one_byte!
+      magic_chars  = [stop_char].compact + %w( { [ ' " )
+      regexp_class_subexpression_openers = '[%s]' % magic_chars.map{|c| Regexp.escape(c) }.join
+      
+      # Match any character that can open a subexpression, or where this parser has to return
+      re = /#{regexp_class_subexpression_openers}/
+      
+      loop do
+        line_start_pos = io.pos
+        line = io.gets
         
-        if char == stop_char # Bail out of a subexpr
-          # Handle any remaining subexpressions
-          return wrap_up(expressions, stack, buf, stack_depth, multiple_expressions)
-        elsif char == " " || char == "\n" # Space
-          if buf.length > 0
-            stack << buf
-            buf = ''
-          end
-          if TERMINATORS.include?(char) && stack.any? && !last_char_was_linebreak # Introduce a stack separator! This is a new line
-            stack << buf if buf.length > 0
-            # Immediately run this expression through the filter
-            filtered_expr = compact_subexpr(stack, stack_depth + 1)
-            stack = []
+        # When the IO eof?s return the expressions
+        return wrap_up(expressions, stack, stack_depth, multiple_expressions) if line.nil?
+        
+        line.chomp!
+        
+        scanner = StringScanner.new(line)
+        until scanner.eos?
+          
+          chunk_with_terminator = scanner.scan_until(re)
+          if chunk_with_terminator
+            # Memorize the position within line
+            restart_scanning_at = line_start_pos + scanner.pos
             
-            # Only preserve the parsed expression if it's not nil
-            expressions << filtered_expr unless filtered_expr.nil?
+            # Grab everything until the latest character and put it onto the stack
+            tokens_to_stack(chunk_with_terminator, stack)
             
-            last_char_was_linebreak = true
-            multiple_expressions = true
-            #puts "Next expression! #{expressions.inspect} #{stack.inspect} #{buf.inspect}"
-          else
-            last_char_was_linebreak = false
+            # char is the magic "special" character at the end of the matched string
+            char = chunk_with_terminator[LAST_CHAR]
+            
+            if char == stop_char # Bail out of a subexpr
+              puts "Bailing out of a subexpr at depth #{stack_depth}"
+              return wrap_up(expressions, stack, stack_depth, multiple_expressions)
+            elsif char == '[' # Opens a new string expression
+              # Rewind the IO to the place where our special expression ended
+              io.seek(restart_scanning_at) 
+              scanner.terminate
+              stack << [:b] + sub_parse(io, ']', stack_depth + 1)
+            elsif char == '{' # Opens a new literal expression  
+              # Rewind the IO to the place where our special expression ended
+              io.seek(restart_scanning_at)
+              scanner.terminate
+              stack << [:c] + sub_parse(io, '}', stack_depth + 1)
+            elsif QUOTES.include?(char)
+              # Rewind the IO to the place where our special expression ended
+              io.seek(restart_scanning_at)
+              scanner.terminate
+              stack << parse_str(io, char)
+            end
+          else # Nothing to do on this str
+            # Scan until the end of str, the scanner will arrive to eos
+            chunk_until_eos = scanner.scan_until(/$/)
+            expressions << stack if stack.any?
+            tokens_to_stack(chunk_until_eos, stack, stack_depth)
           end
-        elsif char == '[' # Opens a new string expression
-          stack << buf if (buf.length > 0)
-          stack << [:b] + sub_parse(io, ']', stack_depth + 1)
-        elsif char == '{' # Opens a new literal expression  
-          stack << buf if (buf.length > 0)
-          stack << [:c] + sub_parse(io, '}', stack_depth + 1)
-        elsif char == '"'
-          stack << buf if (buf.length > 0)
-          stack << parse_str(io, '"')
-        elsif char == "'"
-          stack << buf if (buf.length > 0)
-          stack << parse_str(io, "'")
-        else
-          buf << char
         end
       end
       
-      return wrap_up(expressions, stack, buf, stack_depth, multiple_expressions)
+      return wrap_up(expressions, stack, stack_depth, multiple_expressions)
     end
     
     def chomp!(stack)
       stack.delete_at(-1) if stack.any? && stack[-1].nil?
     end
     
-    def no_eof(&blk)
-      begin
-        loop(&blk)
-      rescue Bychar::EOFError
-      end
-    end
-    
     def parse_str(io, stop_char)
       buf = ''
-      no_eof do
-        c = io.read_one_byte!
+      until io.eof?
+        c = io.read(1)
         if c == stop_char && buf[LAST_CHAR] != ESC
           return buf
         elsif buf[LAST_CHAR] == ESC # Eat out the escape char
