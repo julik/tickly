a441f0a22ca9b06a06c68258fbf24197843159e3
Julik Tarkhanov <me@julik.nl>
2013-03-24 13:42:11 +0100

    Add huge test

--- test/test_node_processor.rb
+++ test/test_node_processor.rb
@@ -3,6 +3,7 @@ require 'helper'
 class TestParserEvaluator < Test::Unit::TestCase
   include Tickly::Emitter
   
+  HUGE_SCRIPT = File.open(File.dirname(__FILE__) + "/test-data/huge_nuke_tcl.tcl")
   NUKE7_SCRIPT = File.open(File.dirname(__FILE__) + "/test-data/nuke7_tracker_2tracks.nk")
   
   class Tracker4
@@ -32,5 +33,20 @@ class TestParserEvaluator < Test::Unit::TestCase
   def test_raises_without_a_block
     pe = Tickly::NodeProcessor.new
     assert_raise(LocalJumpError) { pe.parse(NUKE7_SCRIPT) }
+  end
+  
+  class Tracker3
+    def initialize(n); end
+  end
+  
+  def test_parses_huge
+    pe = Tickly::NodeProcessor.new
+    pe.add_node_handler_class(Tracker3)
+    
+    counter = 0
+    pe.parse(HUGE_SCRIPT) do | t |
+      counter += 1
+    end
+    assert_equal 45, counter
   end
 end
