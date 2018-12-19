3cab675eec290ff8a3a0812ef04c31c4d3b9a590
Julik Tarkhanov <me@julik.nl>
2018-12-19 02:41:58 +0100

    Separate version out

--- lib/tickly.rb
+++ lib/tickly.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + "/tickly/parser"
 require File.dirname(__FILE__) + "/tickly/evaluator"
 require File.dirname(__FILE__) + "/tickly/curve"
 require File.dirname(__FILE__) + "/tickly/node_processor"
+require File.dirname(__FILE__) + "/tickly/version"
 
 module Tickly
-  VERSION = '2.1.6'
 end
---
+++ lib/tickly/version.rb
@@ -0,0 +1,3 @@
+module Tickly
+  VERSION = "2.1.6"
+end
